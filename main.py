76f587f82b3ac73dea35ad9c172fdffe0f62be6d
Captainexpo-1 <captainexpo@gmail.com>
2025-03-28 17:17:08 -0400

    Added slack integration

--- main.py
+++ main.py
@@ -6,18 +6,25 @@ import websockets
 import asyncio
 import dotenv
 import os
+import slackintegration
 
 class PresenceStopException(Exception):
     pass
 
 print(pypresence.__file__)
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(override=True)
 
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 
 connection_start = time.time()
+
+current_song_url = "" 
+
+def panic():
+    if os.environ.get("ENABLE_SLACK", "0") == "1":
+        slackintegration.remove_status()
 
 def connect_rpc():
     global connection_start
@@ -31,6 +38,7 @@ def connect_rpc():
         except Exception as e:
             print("Failed to connect to Discord Rich Presence. Retrying in 5 seconds...")
             print(e)
+            panic()
             time.sleep(5)
 
 rpc = connect_rpc()
@@ -51,9 +59,14 @@ def format_time(seconds: int):
         return f"{seconds}s"
 
 async def handler(websocket):
+    global current_song_url
     async for message in websocket:
         data = json.loads(message)
         print("Received:", data)
+        if data.get("url", "") == current_song_url:
+            print("Ignoring duplicate song URL.")
+            continue
+        current_song_url = data.get("url", "")
         try:
             if data.get("type", "") == "close":
                 if rpc:
@@ -61,6 +74,7 @@ async def handler(websocket):
                     print("Cleared Discord Rich Presence.")
                 else:
                     print("RPC is not initialized. Skipping clear operation.")
+                panic()
                 raise PresenceStopException()
     
             await asyncio.to_thread(
@@ -76,8 +90,11 @@ async def handler(websocket):
                 activity_type=2,
             )
             
+            if os.environ.get("ENABLE_SLACK", "0") == "1":
+                slackintegration.set_song(data['title'])
 
         except BrokenPipeError as e:
+            panic()
             print("Connection broken, unable to update Discord Rich Presence.")
             print(e)
 
@@ -92,10 +109,13 @@ async def start_server():
         except Exception as e:
             print("WebSocket server encountered an error. Retrying in 5 seconds...")
             print(e)
+            panic()
+
             await asyncio.sleep(5)
         except PresenceStopException:
             print("Presence stopped by client.")
             connection_start = 0
+            panic()
             break
             
 
---
+++ slackintegration.py
@@ -0,0 +1,43 @@
+import os
+from slack_sdk import WebClient
+import dotenv
+from typing import Dict
+
+dotenv.load_dotenv(override=True)
+
+SLACK_TOKEN = os.environ.get("SLACK_OAUTH")
+
+client = WebClient(token=SLACK_TOKEN)
+
+def set_song(song_name: str):
+    custom_status = {
+        "status_text": f"Listening to {song_name}",  # Text of the status
+        "status_emoji": ":music:",  # Emoji to display
+        "status_expiration": 0  # Set to 0 for no expiration
+    }
+    set_status(custom_status)
+    
+def remove_status():
+    set_status(
+        {
+            "status_text": "",  # Text of the status
+            "status_emoji": "",  # Emoji to display
+            "status_expiration": 0  # Set to 0 for no expiration
+        }
+    )    
+    
+def set_status(status: Dict[str, str|int]):
+    try:
+        result = client.users_profile_set(
+            user=os.environ.get("SLACK_USER_ID"),  # Replace with your Slack user ID
+            profile=status
+        )
+
+        if result["ok"]:
+            print("Custom status updated successfully!")
+        else:
+            print(f"Error updating status: {result['error']}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
